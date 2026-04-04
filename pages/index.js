e574813567b80fd481ae3cbb2e553485d120be72
thibaubergine <thibaut.sansous@gmail.com>
2026-04-04 14:33:51 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-// This page is not used directly - the app is served from public/index.html
-// via the rewrite in next.config.js
+import fs from 'fs'
+import path from 'path'
+
 export default function Home() {
   return null
+}
+
+export async function getServerSideProps({ res }) {
+  const filePath = path.join(process.cwd(), 'public', 'index.html')
+  const html = fs.readFileSync(filePath, 'utf8')
+  res.setHeader('Content-Type', 'text/html')
+  res.write(html)
+  res.end()
+  return { props: {} }
 }
