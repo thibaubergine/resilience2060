5f48fc123941b6a44e22b346c8533222fa23b36e
thibaubergine <thibaut.sansous@gmail.com>
2026-05-29 19:54:17 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/petroleum/:path*',
+        destination: 'https://petroleum-inky.vercel.app/petroleum/:path*',
+      },
+    ]
+  },
+}
 module.exports = nextConfig
+  nextConfig
