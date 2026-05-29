c65a959d1f71a74b0b196d8c38757dcd7526399c
thibaubergine <thibaut.sansous@gmail.com>
2026-05-29 21:13:36 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,22 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: '/petroleum/assets/:path*',
+        destination: 'https://petroleum-inky.vercel.app/assets/:path*',
+      },
+      {
+        source: '/petroleum/:file.:ext',
+        destination: 'https://petroleum-inky.vercel.app/:file.:ext',
+      },
+      {
+        source: '/petroleum',
+        destination: 'https://petroleum-inky.vercel.app/index.html',
+      },
+      {
         source: '/petroleum/:path*',
-        destination: 'https://petroleum-inky.vercel.app/petroleum/:path*',
+        destination: 'https://petroleum-inky.vercel.app/index.html',
       },
     ]
   },
 }
-module.exports = nextConfig
-  nextConfig
+module.exports = nextConfignextConfig
